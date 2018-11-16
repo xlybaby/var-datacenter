254fc229b6e63125f33650925aee853f2917bdc4
Administrator <Administrator@WIN-J3EQULD7DG7>
2018-11-16 18:09:42 +0800

    20181116

---
+++ src/main/java/com/machintosh1983/var/datacenter/uc/scenario/vo/ScenarioQueryVO.java
@@ -0,0 +1,28 @@
+package com.machintosh1983.var.datacenter.uc.scenario.vo;
+
+public class ScenarioQueryVO {
+
+	private String scenarioId;
+	private int offset;
+	private int limit;
+	public String getScenarioId() {
+		return scenarioId;
+	}
+	public void setScenarioId(String scenarioId) {
+		this.scenarioId = scenarioId;
+	}
+	public int getOffset() {
+		return offset;
+	}
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	public int getLimit() {
+		return limit;
+	}
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	
+	
+}
